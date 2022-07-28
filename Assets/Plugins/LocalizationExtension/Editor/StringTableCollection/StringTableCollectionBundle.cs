d0bbaf6ff701a0726e96e5c6bf226a77c2e654f6
tsgcpp <tsgcpp@users.noreply.github.com>
2022-07-28 22:30:03 +0900

    feat: Add pull and push methods in "StringTableCollectionBundle"

--- Assets/Plugins/LocalizationExtension/Editor/StringTableCollection/StringTableCollectionBundle.cs
+++ Assets/Plugins/LocalizationExtension/Editor/StringTableCollection/StringTableCollectionBundle.cs
@@ -1,15 +1,22 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.Localization;
 using UnityEditor.Localization.Plugins.Google;
 using UnityEditor.Localization.Reporting;
 
-namespace Tsgcpp.Localization.Extension.Editor
+namespace Tsgcpp.Localization.Extension.Editor.Google
 {
     [CreateAssetMenu(fileName = nameof(StringTableCollectionBundle), menuName = "Tsgcpp/Localization Extension/" + nameof(StringTableCollectionBundle), order = 1)]
     public sealed class StringTableCollectionBundle : ScriptableObject, ITargetFoldersProvider
     {
+        [Tooltip("SheetsServiceProvider for pull and push.")]
+        [SerializeField] private SheetsServiceProvider _sheetsServiceProvider;
+        public SheetsServiceProvider SheetsServiceProvider => _sheetsServiceProvider;
+
+        [Tooltip("The folders to search for StringTableCollection assets.")]
         [SerializeField] private List<DefaultAsset> _targetFolders;
         public IReadOnlyList<DefaultAsset> TargetFolders => _targetFolders;
 
@@ -17,11 +24,24 @@ namespace Tsgcpp.Localization.Extension.Editor
             AssetFinding.FindAssetsInFolders<StringTableCollection>(TargetFolders);
 
         public void PullAllLocales(
+            bool removeMissingEntries = false,
+            ITaskReporter reporter = null,
+            bool createUndo = false)
+        {
+            PullAllLocales(
+                serviceProvider: SheetsServiceProvider,
+                removeMissingEntries: removeMissingEntries,
+                reporter: reporter,
+                createUndo: createUndo);
+        }
+
+        public void PullAllLocales(
             IGoogleSheetsService serviceProvider,
             bool removeMissingEntries = false,
             ITaskReporter reporter = null,
             bool createUndo = false)
         {
+            ValidateProperties();
             StringTableCollections.PullAllLocales(
                 serviceProvider: serviceProvider,
                 removeMissingEntries: removeMissingEntries,
@@ -29,15 +49,39 @@ namespace Tsgcpp.Localization.Extension.Editor
                 createUndo: createUndo);
         }
 
+        public void PushAllLocales(ITaskReporter reporter = null)
+        {
+            PushAllLocales(
+                serviceProvider: SheetsServiceProvider,
+                reporter: reporter);
+        }
+
         public void PushAllLocales(
             IGoogleSheetsService serviceProvider,
-            bool removeMissingEntries = false,
-            ITaskReporter reporter = null,
-            bool createUndo = false)
+            ITaskReporter reporter = null)
         {
+            ValidateProperties();
             StringTableCollections.PushAllLocales(
                 serviceProvider: serviceProvider,
                 reporter: reporter);
+        }
+
+        private void ValidateProperties()
+        {
+            if (TargetFolders == null)
+            {
+                throw new NullReferenceException("TargetFolders is null.");
+            }
+
+            if (TargetFolders.Any(folder => folder == null))
+            {
+                throw new NullReferenceException("TargetFolders contains null.");
+            }
+
+            if (TargetFolders.Any(folder => !AssetDatabase.IsValidFolder(AssetDatabase.GetAssetPath(folder))))
+            {
+                throw new ArgumentException("TargetFolders contains invalid folder.");
+            }
         }
     }
 }
