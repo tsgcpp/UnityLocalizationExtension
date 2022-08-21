0a40ad54980cccea782bd0d0ec763a45445b5453
tsgcpp <tsgcpp@users.noreply.github.com>
2022-08-21 15:39:08 +0900

    fix: Rename the method to "FindAssets"
    Fix the method to return no assets if the argument is null
    Add "FindAsset" with a "DefaultAsset"

--- Assets/Tests/LocalizationExtension/Editor/AssetTool/TestAssetFinding.cs
+++ Assets/Tests/LocalizationExtension/Editor/AssetTool/TestAssetFinding.cs
@@ -12,7 +12,35 @@ namespace Tests.Tsgcpp.Localization.Extension.Editor
     public class TestAssetFinding
     {
         [Test]
-        public void FindAssetsInFolders_ReturnsTextAsset_IfFolderContainsTextAsset_WhenOneFolder()
+        public void FindAssets_WithDefaultAssetArg_ReturnsAsset_IfFolderContainsAsset()
+        {
+            // Arrange
+            var folder = AssetDatabase.LoadAssetAtPath<DefaultAsset>("Assets/Tests/LocalizationExtension/Editor/Resources/Foo02/Bar01/Baz01");
+
+            // Act
+            var actual = AssetFinding.FindAssets<TestAsset>(folder);
+
+            // Assert
+            Assert.That(actual.Count, Is.EqualTo(2));
+            Assert.That(actual[0].Label, Is.EqualTo("02"));
+            Assert.That(actual[1].Label, Is.EqualTo("04"));
+        }
+
+        [Test]
+        public void FindAssets_WithDefaultAssetArg_ReturnsNoAssets_IfArgIsNull()
+        {
+            // Arrange
+            DefaultAsset folder = null;
+
+            // Act
+            var actual = AssetFinding.FindAssets<TestAsset>(folder);
+
+            // Assert
+            Assert.That(actual.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FindAssets_WithListArg_ReturnsAsset_IfFolderContainsAsset_WhenOneFolder()
         {
             // Arrange
             var folders = new List<DefaultAsset>
@@ -21,16 +49,16 @@ namespace Tests.Tsgcpp.Localization.Extension.Editor
             };
 
             // Act
-            var actual = AssetFinding.FindAssetsInFolders<TextAsset>(folders);
+            var actual = AssetFinding.FindAssets<TestAsset>(folders);
 
             // Assert
             Assert.That(actual.Count, Is.EqualTo(2));
-            Assert.That(actual[0].text, Is.EqualTo("02"));
-            Assert.That(actual[1].text, Is.EqualTo("04"));
+            Assert.That(actual[0].Label, Is.EqualTo("02"));
+            Assert.That(actual[1].Label, Is.EqualTo("04"));
         }
 
         [Test]
-        public void FindAssetsInFolders_ReturnsTextAsset_IfFolderContainsTextAsset_WhenMultipleFolders()
+        public void FindAssets_WithListArg_ReturnsAsset_IfFolderContainsAsset_WhenMultipleFolders()
         {
             // Arrange
             var folders = new List<DefaultAsset>
@@ -41,18 +69,18 @@ namespace Tests.Tsgcpp.Localization.Extension.Editor
             };
 
             // Act
-            var actual = AssetFinding.FindAssetsInFolders<TextAsset>(folders);
+            var actual = AssetFinding.FindAssets<TestAsset>(folders);
 
             // Assert
             Assert.That(actual.Count, Is.EqualTo(4));
-            Assert.That(actual[0].text, Is.EqualTo("01"));
-            Assert.That(actual[1].text, Is.EqualTo("02"));
-            Assert.That(actual[2].text, Is.EqualTo("04"));
-            Assert.That(actual[3].text, Is.EqualTo("03"));
+            Assert.That(actual[0].Label, Is.EqualTo("01"));
+            Assert.That(actual[1].Label, Is.EqualTo("02"));
+            Assert.That(actual[2].Label, Is.EqualTo("04"));
+            Assert.That(actual[3].Label, Is.EqualTo("03"));
         }
 
         [Test]
-        public void FindAssetsInFolders_ReturnsUniquedTextAsset_IfTextAssetsAreDupulicated()
+        public void FindAssets_WithListArg_ReturnsUniquedAsset_IfAssetsAreDupulicated()
         {
             // Arrange
             var folders = new List<DefaultAsset>
@@ -63,17 +91,17 @@ namespace Tests.Tsgcpp.Localization.Extension.Editor
             };
 
             // Act
-            var actual = AssetFinding.FindAssetsInFolders<TextAsset>(folders);
+            var actual = AssetFinding.FindAssets<TestAsset>(folders);
 
             // Assert
             Assert.That(actual.Count, Is.EqualTo(3));
-            Assert.That(actual[0].text, Is.EqualTo("02"));
-            Assert.That(actual[1].text, Is.EqualTo("04"));
-            Assert.That(actual[2].text, Is.EqualTo("03"));
+            Assert.That(actual[0].Label, Is.EqualTo("02"));
+            Assert.That(actual[1].Label, Is.EqualTo("04"));
+            Assert.That(actual[2].Label, Is.EqualTo("03"));
         }
 
         [Test]
-        public void FindAssetsInFolders_ReturnsNoTextAssets_IfTextAssetsDontExist()
+        public void FindAssets_WithListArg_ReturnsNoAssets_IfAssetsDontExist()
         {
             // Arrange
             var folders = new List<DefaultAsset>
@@ -82,14 +110,43 @@ namespace Tests.Tsgcpp.Localization.Extension.Editor
             };
 
             // Act
-            var actual = AssetFinding.FindAssetsInFolders<TextAsset>(folders);
+            var actual = AssetFinding.FindAssets<TestAsset>(folders);
 
             // Assert
             Assert.That(actual.Count, Is.EqualTo(0));
         }
 
         [Test]
-        public void FindAssetsInFolders_ReturnsNoTextAssets_IfFoldersIsNone()
+        public void FindAssets_WithListArg_ReturnsNoAssets_IfNullList()
+        {
+            // Arrange
+            var folders = new List<DefaultAsset>
+            {
+                null,
+            };
+
+            // Act
+            var actual = AssetFinding.FindAssets<TestAsset>(folders);
+
+            // Assert
+            Assert.That(actual.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FindAssets_WithListArg_ReturnsNoAssets_IfArgIsNull()
+        {
+            // Arrange
+            List<DefaultAsset> folders = null;
+
+            // Act
+            var actual = AssetFinding.FindAssets<TestAsset>(folders);
+
+            // Assert
+            Assert.That(actual.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FindAssets_WithListArg_ReturnsNoAssets_IfFoldersIsNone()
         {
             // Arrange
             var folders = new List<DefaultAsset>
@@ -97,16 +154,10 @@ namespace Tests.Tsgcpp.Localization.Extension.Editor
             };
 
             // Act
-            var actual = AssetFinding.FindAssetsInFolders<TextAsset>(folders);
+            var actual = AssetFinding.FindAssets<TestAsset>(folders);
 
             // Assert
             Assert.That(actual.Count, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void FindAssetsInFolders_ThrowsNullReferenceException_IfArgIsNull()
-        {
-            Assert.Throws<NullReferenceException>(() => AssetFinding.FindAssetsInFolders<TextAsset>(null));
         }
     }
 }
